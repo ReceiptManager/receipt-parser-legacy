23dd94ba64961a611d61f7882e29ebd846961c9a
Matthias Endler <matthias.endler@trivago.com>
2015-11-09 15:54:40 +0100

    Move all settings into a config file. Simplify market name detection.

---
+++ objectview.py
@@ -0,0 +1,3 @@
+class objectview(object):
+    def __init__(self, d):
+        self.__dict__ = d
--- parser.py
+++ parser.py
@@ -7,32 +7,14 @@ from os import listdir
 from os.path import isfile, join
 from difflib import get_close_matches
 from collections import defaultdict
-
-# Where the receipts are stored
-# Receipts should be simple text files
-receipts_path = "txt"
-
-# An ugly regex to match dates like 19.08.15 19. 08. 2015
-date_format = '.*?(?P<date>(\d{2,4}(\.\s?|[^a-zA-Z\d])\d{2}(\.\s?|[^a-zA-Z\d])(20)?1[3-6]))\s+'
+from objectview import objectview
+import yaml
 
 
 class Receipt(object):
-    def __init__(self, raw):
+    def __init__(self, config, raw):
+        self.config = config
         self.market = self.date = self.sum = None
-        # Market names roughly ordered by likelyhood.
-        self.markets = [
-            "penny", "p e n n y", "rewe", "real", "netto",
-            "kaiser", "aldi", "drogerie", "kodi", "sb warenhaus", "lidl", "shell",
-            "aral", "total", "m a r k t gmbh", "RK Tankstellen", "Schuhhaus"
-        ]
-        # A number of market locations for fuzzy parsing
-        self.market_locations = {
-            'kaiserswerther straße 270': "kaiser",
-            'friedrichstr 128—133': "aldi"
-        }
-        # Sum keys roughly ordered by likelyhood.
-        self.sum_keys = ["summe", "gesamtbetrag", "gesamt", "total", "sum.",
-                         "zwischensumme", "bar"]
         self.raw = map(str.lower, raw)
         self.raw = [line.decode('utf-8') for line in self.raw]
         self.parse()
@@ -58,54 +40,46 @@ class Receipt(object):
 
     def parse_date(self):
         for line in self.raw:
-            m = re.match(date_format, line)
+            m = re.match(self.config.date_format, line)
             if m:
                 # We're happy with the first match for now
                 return m.group(1)
 
     def parse_market(self):
-        for line in self.raw:
-            for market in self.markets:
-                if market in line:
-                    # Hooray!
-                    return market
-        # Errm... no direct match.
-        # We need to do fuzzy matching or so
-        for market in self.markets:
-            line = self.fuzzy_find(market)
-            if line:
-                return market
-        return self.parse_market_by_location()
-
-    def parse_market_by_location(self):
-        for location, market in self.market_locations.items():
-            if self.fuzzy_find(location, 0.4):
-                return market
+        for int_accuracy in range(10, 6, -1):
+            accuracy = int_accuracy / 10.0
+            for market, spellings in self.config.markets.items():
+                for spelling in spellings:
+                    line = self.fuzzy_find(spelling, accuracy)
+                    if line:
+                        print line, accuracy, market
+                        return market
 
     def parse_sum(self):
-        for sum_key in self.sum_keys:
+        for sum_key in self.config.sum_keys:
             sum_line = self.fuzzy_find(sum_key)
             if sum_line:
                 # Replace all commas with a dot to make
                 # finding and parsing the sum easier
                 sum_line = sum_line.replace(',', '.')
                 # Parse the sum
-                sum_float = re.search('\d+(\.\s?|,\s?|[^a-zA-Z\d])\d{2}', sum_line)
+                sum_float = re.search(self.config.sum_format, sum_line)
                 if sum_float:
                     return sum_float.group(0)
 
 
 def main():
-    receipt_files = [f for f in listdir(receipts_path) if isfile(join(receipts_path, f))]
+    config = read_config()
+    receipt_files = [f for f in listdir(config.receipts_path) if isfile(join(config.receipts_path, f))]
     # Ignore hidden files like .DS_Store
     receipt_files = [f for f in receipt_files if not f.startswith('.')]
     stats = defaultdict(int)
 
     for receipt_file in receipt_files:
-        receipt_path = join(receipts_path, receipt_file)
+        receipt_path = join(config.receipts_path, receipt_file)
         with open(receipt_path) as receipt:
             lines = receipt.readlines()
-            receipt = Receipt(lines)
+            receipt = Receipt(config, lines)
             print(receipt_path, receipt.market, receipt.date, receipt.sum)
             stats["total"] += 1
             if receipt.market:
@@ -115,6 +89,18 @@ def main():
             if receipt.sum:
                 stats["sum"] += 1
     statistics(stats, False)
+
+
+def read_config():
+    stream = open("config.yml", "r")
+    docs = yaml.safe_load(stream)
+    """
+    for doc in docs:
+        for k,v in doc.items():
+            print(k, "->", v)
+        print("\n")
+    """
+    return objectview(docs)
 
 
 def statistics(stats, write=False):
