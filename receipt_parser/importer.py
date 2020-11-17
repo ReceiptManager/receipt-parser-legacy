bb6108955296754513954a848da78a2893736c69
William Todt <william.todt@mailbox.org>
2020-11-17 16:22:31 +0100

    Ignore python Byte-compiled python programs.

--- receipt_parser/importer.py
+++ receipt_parser/importer.py
@@ -17,8 +17,6 @@
 import io
 import os
 
-import cv2
-import numpy as np
 from PIL import Image
 from pytesseract import pytesseract
 from wand.image import Image as WandImage
@@ -74,45 +72,12 @@ def rotate_image(input_file, output_file, angle=90):
         Rotates image and saves result
     """
     print("Rotate image: ", input_file, " ~> ", output_file)
-    image = cv2.imread(input_file)
-    # convert the image to grayscale and flip the foreground
-    # and background to ensure foreground is now "white" and
-    # the background is "black"
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    gray = cv2.bitwise_not(gray)
-    # threshold the image, setting all foreground pixels to
-    # 255 and all background pixels to 0
-    thresh = cv2.threshold(gray, 0, 255,
-        cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
 
-    # grab the (x, y) coordinates of all pixel values that
-    # are greater than zero, then use these coordinates to
-    # compute a rotated bounding box that contains all
-    # coordinates
-    coords = np.column_stack(np.where(thresh > 0))
-    angle = cv2.minAreaRect(coords)[-1]
-    # the `cv2.minAreaRect` function returns values in the
-    # range [-90, 0); as the rectangle rotates clockwise the
-    # returned angle trends to 0 -- in this special case we
-    # need to add 90 degrees to the angle
-    #if angle < -45:
-    #    angle = -(90 + angle)
-    # otherwise, just take the inverse of the angle to make
-    # it positive
-   # else:
-     #   angle = -angle
+    with WandImage(filename=input_file) as img:
+        with img.clone() as rotated:
+            rotated.rotate(angle)
+            rotated.save(filename=output_file)
 
-    # rotate the image to deskew it
-    (h, w) = image.shape[:2]
-    center = (w // 2, h // 2)
-    M = cv2.getRotationMatrix2D(center, angle, 1.0)
-    rotated = cv2.warpAffine(image, M, (w, h),
-                             flags=cv2.INTER_CUBIC, borderMode=cv2.BORDER_REPLICATE)
-
-    cv2.imshow("Input", image)
-    cv2.imshow("Rotated", rotated)
-    cv2.waitKey(0)
-    print("[INFO] angle: {:.3f}".format(angle))
 
 def sharpen_image(input_file, output_file):
     """
